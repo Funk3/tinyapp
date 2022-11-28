0b758b7d852dc56097e17ff93e520820faccf259
Zak <zak.johansson@gmail.com>
2022-11-28 15:54:37 -0800

    implemented redirect

--- express_server.js
+++ express_server.js
@@ -6,9 +6,16 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  let longUrl = ""; 
+  longUrl += generateRandomString()
+  urlDatabase[longUrl] = req.body.longURL; // Log the POST request body to the console
+  res.redirect(`/u/:${longUrl}`); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/", (req, res) => {
@@ -24,9 +31,13 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new")
 });
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: href="#"};
+app.get("/u/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: href="#"}
   res.render("urls_show", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
@@ -37,4 +48,15 @@ app.listen(PORT, () => {
 
 
 
-function generateRandomString() {}
+function generateRandomString() {
+  let result      = '';
+  const char      = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwyxz1234567890" 
+  const length    = char.length
+
+  for (i = 0; i < 6; i++) {
+    result += char.charAt(Math.floor(Math.random() * length));
+  }
+  
+  return result;
+};
+
